0ee552e34e959408739ab705c3a1d83f81bed7ba
g3rm4n <geavak@taltech.ee>
2025-02-05 20:46:56 +0200

    added UpdateOrderStatusRequest

---
+++ Models/Requests/UpdateOrderStatusRequest.cs
@@ -0,0 +1,8 @@
+// Models/Requests/UpdateOrderStatusRequest.cs
+namespace OnlineShoppingSite.Models.Requests
+{
+    public class UpdateOrderStatusRequest
+    {
+        public string Status { get; set; }
+    }
+}
